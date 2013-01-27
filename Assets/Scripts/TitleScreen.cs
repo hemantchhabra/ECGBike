7d6f37111e2d56dae2f403556b06c7909d8961ef
Ekbote, Chirantan <ekbote@seas.harvard.edu>
2013-01-26 22:25:30 -0500

    Added title screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnGUI() {
+		if (GUI.Button(new Rect(Screen.width*0.5f - 48, Screen.height * 0.5f - 24, 96, 48), "Start!")) { 
+			Application.LoadLevel(1); 	
+		}
+	}
+}
