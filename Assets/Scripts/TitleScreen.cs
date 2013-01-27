ee610cfd6c191b3a54142fd987d6b8f75f2abfa9
Shawn Conrad <shawnrad@mit.edu>
2013-01-27 14:23:14 -0500

    Instructions on screen
    Added instructions for Unity Player & iPad/Pod

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -9,6 +9,23 @@ public class TitleScreen : MonoBehaviour {
 	}
 	
 	void OnGUI() {
+
+	int instrHeight = 24;
+	int instrWidth1 = 260;
+	int instrWidth2 = 330;
+	string clockwiseControl;
+	string counterClockwiseControl;
+#if UNITY_IPHONE
+	clockwiseControl = "HERE";
+	counterClockwiseControl = "HERE";
+#else
+	clockwiseControl = "A";
+	counterClockwiseControl = "D";
+#endif
+		
+	GUI.Box(new Rect(0,Screen.height-instrHeight,instrWidth1,instrHeight),"PRESS "+clockwiseControl+" TO ROTATE CLOCKWISE");
+	GUI.Box(new Rect(Screen.width-instrWidth2,Screen.height-instrHeight,instrWidth2,instrHeight),"PRESS "+counterClockwiseControl+" TO ROTATE COUNTER-CLOCKWISE");
+		
 		if (GUI.Button(new Rect(Screen.width*0.5f - 48, Screen.height * 0.5f + 96, 96, 48), "Start!")) { 
 			Application.LoadLevel(1); 	
 		}
